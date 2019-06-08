73b23553117e37a1583b03bdde6ce9e75797a2c5
Rikki Tooley <hello@rikk.it>
2019-06-08 15:26:53 +0100

    Move scraping into util file

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import { getSets, IGlastoSet } from './utils/scraper';
 import './App.css';
 
-import Getsy from "getsy";
-
-const getSets = async () => {
-  const setsAzUrl = "https://www.glastonburyfestivals.co.uk/line-up/line-up-2019/?artist";
-  const page = await Getsy(setsAzUrl, { corsProxy: "https://cors-anywhere.herokuapp.com/" });
-  const rows = page.getMe(".lineup .letterList > li");
-  
-  const sets = [];
-  for (const row of rows) {
-    const title = row.getElementsByClassName("title")[0].textContent; // TODO there's a link here sometimes
-    const stage = row.getElementsByClassName("stage")[0].textContent;
-    const day = row.getElementsByClassName("day")[0].textContent;
-    const times = row.getElementsByClassName("end")[0].textContent; // HH:mm - HH:mm
-
-    sets.push({ title, stage, day, times });
-  }
-
-  for (const set of sets) {
-    console.log(set);
-  }
-}
-
-getSets();
 
 const App: React.FC = () => {
+  const [allSets, setSetTimes] = useState<IGlastoSet[]>([]);
+
+  useEffect(() => {
+    getSets().then(sets => setSetTimes(sets));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <ul>
+        {allSets.map(set => (
+          <li>
+            {set.title} ({set.stage})
+            {set.startTime && set.startTime.toLocaleString()} - {set.endTime && set.endTime.toLocaleString()}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/utils/scraper.ts
@@ -0,0 +1,91 @@
+
+import Getsy from "getsy";
+
+type GlastoDay = "WEDNESDAY" | "THURSDAY" | "FRIDAY" | "SATURDAY" | "SUNDAY";
+
+export interface IGlastoSet {
+  title: string;
+  stage: string;
+  startTime: Date | null;
+  endTime: Date | null; // null == TBA
+}
+
+const glastoDayToDate = (day: string): Date => {
+  day = day.toUpperCase() as GlastoDay;
+  switch (day) {
+    case "WEDNESDAY":
+      return new Date(2019, 6, 26);
+    case "THURSDAY":
+      return new Date(2019, 6, 27);
+    case "FRIDAY":
+      return new Date(2019, 6, 28);
+    case "SATURDAY":
+      return new Date(2019, 6, 29);
+    case "SUNDAY":
+      return new Date(2019, 6, 30);
+    default:
+      throw new Error(`Unhandled day ${day}`);
+  }
+}
+
+const timeOfDayToMs = (HHmm: string = ""): number | null => {
+  const match = HHmm.match(/(\d\d):(\d\d)/);
+  if (match) {
+    const [_, HH, mm] = match;
+    const hoursInMs = parseInt(HH) * 60 * 60 * 1000;
+    const minutesInMs = parseInt(mm) * 60 * 1000;
+    return hoursInMs + minutesInMs;
+  }
+  else {
+    return null;
+  }
+}
+
+const parseRowElement = (row: HTMLElement): IGlastoSet => {
+  const [titleEl, stageEl, dayEl, timesEl] = row.children;
+
+  const title = titleEl.textContent!;
+  const stage = stageEl.textContent!;
+
+  const matches = timesEl.textContent!.match(/(\d\d:\d\d)/g);
+  if (matches) {
+    const [setStart, setEnd] = matches;
+
+    const setStartTs = timeOfDayToMs(setStart);
+    let startTime: Date | null = null;
+    let endTime: Date | null = null;
+    if (setStartTs != null) {
+      const startTimeTs = glastoDayToDate(dayEl.textContent!).valueOf() + setStartTs;
+      startTime = new Date(startTimeTs);
+
+      const setEndTs = timeOfDayToMs(setEnd);
+      if (setEndTs != null) {
+        const setLengthTs = setEndTs > setStartTs
+          ? setEndTs - setStartTs // Same day
+          : (24 * 60 * 60 * 1000 - setStartTs) + setEndTs; // Crosses midnight
+        endTime = new Date(startTimeTs + setLengthTs);
+      }
+    }
+
+    return { title, stage, startTime, endTime };
+  }
+  else {
+    return { title, stage, startTime: null, endTime: null };
+  }
+}
+
+export const getSets = async (): Promise<IGlastoSet[]> => {
+  const setsAzUrl = "https://www.glastonburyfestivals.co.uk/line-up/line-up-2019/?artist";
+  const page = await Getsy(setsAzUrl, { corsProxy: "https://cors-anywhere.herokuapp.com/" });
+  const rows = page.getMe(".lineup .letterList > li");
+
+  const sets: IGlastoSet[] = [];
+  for (let i = 0; i < rows.length; i++) {
+    sets.push(parseRowElement(rows[i]));
+  }
+
+  return sets.sort((a, b) => a.startTime && b.startTime
+    ? a.startTime.valueOf() - b.startTime.valueOf()
+    : -1
+  );
+}
